d679dadb3be04f236e7f9ddc14e6180e6b332d9e
Youssef Nafaa <nafaayoussef@gmail.com>
2020-12-09 14:33:56 -0500

    Web App Calculator

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCalc1YoussefNAFAA.Models;
+
+namespace WebAppCalc1YoussefNAFAA.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View(new Calculator());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calculator calc, string calculate)
+        {
+            calc.total = 0;
+            switch (calculate)
+            {
+                case "add":
+                    calc.total = calc.number1 + calc.number2;
+                    break;
+                case "substract":
+                    calc.total = calc.number1 - calc.number2;
+                    break;
+                case "multiply":
+                    calc.total = calc.number1 * calc.number2;
+                    break;
+                case "divide":
+                    if (calc.number2 == 0)
+                    {
+                        
+                        calc.message = "Division by Zero ";
+                    }
+                    else
+                    {
+                        calc.total = calc.number1 / calc.number2;
+                    }
+                    break;
+                default:
+                    
+                    break;
+            }
+            return View(calc);
+        }
+
+    }
+}
---
+++ Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppCalc1YoussefNAFAA.Models
+{
+    public class Calculator
+    {
+        public double number1 { get; set; }
+        public double number2 { get; set; }
+        public double total { get; set; }
+
+        public string message { get; set; }
+
+    }
+}
